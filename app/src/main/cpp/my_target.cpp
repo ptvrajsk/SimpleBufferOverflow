c78832bb3d0cf6e47df56a6b118ec6c2bec297d9
Prithivi Raj <ptvrajsk@gmail.com>
2020-06-17 13:35:10 +0000

    Optimize my_target.cpp

--- app/src/main/cpp/my_target.cpp
+++ app/src/main/cpp/my_target.cpp
@@ -7,14 +7,10 @@
 
 extern "C" int LLVMFuzzerTestOneInput(const char* Data, size_t size) {
     if (size >= 1) {
-        char* workingData = (char*)malloc(sizeof(char)*size);
-        for (int i = 0; i < size-1; i ++) {
-            workingData[i] = Data[i];
+        if (Data[size-1] == '\0') {
+            char* a = duplicateString(Data);
+            delete[] a;    
         }
-        workingData[size-1] = '\0';
-        char* a = duplicateString(workingData);
-        delete[] a;
-        free(workingData);
     }
     return 0;
 }
